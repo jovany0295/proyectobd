83ce20952dda7a073fae7735bf50bf11d0ba6f5b
Xiada-GR <xgutiereez@gmail.com>
2022-12-16 08:52:30 -0600

    cambios finals

--- asist/src/App/index.js
+++ asist/src/App/index.js
@@ -11,30 +11,38 @@ import Header from '../components/Header'
 import routes from '../router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from '../pages/Login'
+import PageNotFound from '../components/PageNotFound/PageNotFound'
 
 const isLogged = sessionStorage.getItem('token');
-const NotFoundPage = () => {
-  return (
-    <div>
-      <h1 style={{ color: "red", fontSize: 100 }}>404</h1>
-      <h3>Page Not Found</h3>
-      <p>
-        <Link to="/Home">Go Home</Link>
-      </p>
-    </div>
-  );
-};
+// const NotFoundPage = () => {
+//   return (
+//     <div>
+//       <h1 style={{ color: "red", fontSize: 100 }}>404</h1>
+//       <h3>Page Not Found</h3>
+//       <p>
+//         <Link to="/Home">Go Home</Link>
+//       </p>
+//     </div>
+//   );
+// };
+
+
+
 
 const App = () => {
-  
+  if(window.location.href == "http://localhost:3000/login" && isLogged) {
+    window.location.href = './Home';
+    return;
+  }
     return(
       <>
+      
         <BrowserRouter>
           { window.location.href !== "http://localhost:3000/login" ? <Header /> : null }
-
+          
           <Routes>
               <Route path="/login"  element={<Login />} />
-              <Route path="*" element={<NotFoundPage />} />
+              <Route path="*" element={<PageNotFound />} />
               {
                 routes.map( (route, index) => (
                   <Route 
--- asist/src/pages/Home/containers/Greetings/index.js
+++ asist/src/pages/Home/containers/Greetings/index.js
@@ -10,8 +10,54 @@ const Greetings = () => {
     <>
    
    <Typography variant="h5" sx={{ mb: 1 , marginLeft:10}}>
-         Bienvenido
+        
         </Typography>
+        <div class="d-flex d-flex justify-content-center">
+          <div class="welcome">
+              <div class="content rounded-3 p-3">
+                <h1 class="fs-3">Bienvenido a AlfaMaravilla</h1>
+                <p class="mb-0">Hola Equipo, alfa buena maravilla onda dinamita escuadron lobo !</p>
+              </div>
+          </div>          
+        </div>
+    <section class="statistics mt-4">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="box d-flex rounded-2 align-items-center mb-4 mb-lg-0 p-3">
+            <i class="uil-envelope-shield fs-2 text-center bg-primary rounded-circle"></i>
+            <div class="ms-3">
+              <div class="d-flex align-items-center">
+                <h3 class="mb-0">1,245</h3> <span class="d-block ms-2">Emails</span>
+              </div>
+              <p class="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="box d-flex rounded-2 align-items-center mb-4 mb-lg-0 p-3">
+            <i class="uil-file fs-2 text-center bg-danger rounded-circle"></i>
+            <div class="ms-3">
+              <div class="d-flex align-items-center">
+                <h3 class="mb-0">34</h3> <span class="d-block ms-2">Projects</span>
+              </div>
+              <p class="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="box d-flex rounded-2 align-items-center p-3">
+            <i class="uil-users-alt fs-2 text-center bg-success rounded-circle"></i>
+            <div class="ms-3">
+              <div class="d-flex align-items-center">
+                <h3 class="mb-0">5,245</h3> <span class="d-block ms-2">Users</span>
+              </div>
+              <p class="fs-normal mb-0">Lorem ipsum dolor sit amet</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
        
     </>
 
